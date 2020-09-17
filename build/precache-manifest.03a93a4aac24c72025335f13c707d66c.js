4d77b10aba72793fbd0609d136f47bb0a4d7f4d4
Drussell15 <62867827+Drussell15@users.noreply.github.com>
2020-09-16 18:25:54 -0700

    new build
    build

---
+++ build/precache-manifest.03a93a4aac24c72025335f13c707d66c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8e2fd216f93ff053fb78a51d9f4c7ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebfaa14a602ac81a0b2e",
+    "url": "/static/css/main.c26464eb.chunk.css"
+  },
+  {
+    "revision": "be369d8cd1122565e2ee",
+    "url": "/static/js/2.caf06e29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.caf06e29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebfaa14a602ac81a0b2e",
+    "url": "/static/js/main.95575063.chunk.js"
+  },
+  {
+    "revision": "ed0fae93a1705cfe61bf",
+    "url": "/static/js/runtime-main.61c3aabd.js"
+  }
+]);
